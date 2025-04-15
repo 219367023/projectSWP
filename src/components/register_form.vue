57e563694ca64e5874f99ad45c143e481672772e
hluleko <219367023@tut4life.ac.za>
2025-04-15 11:14:04 +0200

    2nd

--- src/components/register_form.vue
+++ src/components/register_form.vue
@@ -27,7 +27,7 @@
       </div>
 
       <div class="form-group">
-        <input v-model="user.dietary_restrictions" placeholder="E.g., Vegetarian, Halal" />
+        <input v-model="user.dietary_restrictions" placeholder="Diet restrictions()" />
       </div>
 
       <div class="form-group">
